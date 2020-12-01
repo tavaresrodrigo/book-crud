73dab45494a107e02b106de87a312fc61fa626aa
rodrigo.actavares@gmail.com <rodrigo.actavares@gmail.com>
2020-12-01 23:50:43 +0000

    fixing add-book render method

--- react-frontend/src/components/add-book.component.js
+++ react-frontend/src/components/add-book.component.js
@@ -66,6 +66,49 @@ export default class AddBook extends Component {
   }
 
   render() {
-    // ...
+    return (
+      <div className="submit-form">
+        {this.state.submitted ? (
+          <div>
+            <h4>You submitted successfully!</h4>
+            <button className="btn btn-success" onClick={this.newBook}>
+              Add
+            </button>
+          </div>
+        ) : (
+          <div>
+            <div className="form-group">
+              <label htmlFor="title">Title</label>
+              <input
+                type="text"
+                className="form-control"
+                id="title"
+                required
+                value={this.state.title}
+                onChange={this.onChangeTitle}
+                name="title"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="description">Description</label>
+              <input
+                type="text"
+                className="form-control"
+                id="description"
+                required
+                value={this.state.description}
+                onChange={this.onChangeDescription}
+                name="description"
+              />
+            </div>
+
+            <button onClick={this.saveBook} className="btn btn-success">
+              Submit
+            </button>
+          </div>
+        )}
+      </div>
+    );
   }
 }

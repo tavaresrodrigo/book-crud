b785732c9c5b1fa608a866f3a8525f15718dd107
Rodrigo Tavares <rodrigo.actavares@gmail.com>
2020-12-02 12:51:48 +0000

    Delete db.config.js

--- nem-backend/app/config/db.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    //url: "mongodb://localhost:27017/bezkoder_db"
-    url: "mongodb+srv://sean:yhXILpC45HYdLqxA@cluster0.g1ksv.mongodb.net/book-crud?retryWrites=true&w=majority"
-  };

3a8e6afaefc1595500376c99e61b31019733ff5f
rodrigo.actavares@gmail.com <rodrigo.actavares@gmail.com>
2020-12-03 15:57:57 +0000

    Configuring authentication

---
+++ nem-backend/app/controllers/auth.controller.js
@@ -0,0 +1,24 @@
+const db = require("../models");
+const User = db.users; 
+
+exports.middlewareAuth = (req, res, next) => {
+  const id = req.query.id;
+  const password = req.query.password;
+  User.findById(id)
+    .then(data => {
+      if (data.password == password) {
+        next()
+    } else {
+        res.status(403).send({
+          message:
+            err.message || "Bad Password"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(403).send({
+        message:
+          err.message || "Unauthorized 403"
+      });
+    });
+}
--- nem-backend/app/routes/users.routes.js
+++ nem-backend/app/routes/users.routes.js
@@ -1,5 +1,6 @@
 module.exports = app => {
   const Users = require("../controllers/User.controller");
+  const Auth = require("../controllers/auth.controller")
 
   var router = require("express").Router();
 
@@ -24,6 +25,8 @@ module.exports = app => {
   // Delete a user
   router.delete("/", Users.deleteAll);
 
-  app.use("/api/users", router);
+  app.use("/api/users", Auth.middlewareAuth, router);
+
+
 };
   

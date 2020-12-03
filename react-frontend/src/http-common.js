c777e325e1ceb65b994271856af918c179580732
rodrigo.actavares@gmail.com <rodrigo.actavares@gmail.com>
2020-12-03 17:50:09 +0000

    httpcommmon

--- react-frontend/src/http-common.js
+++ react-frontend/src/http-common.js
@@ -1,19 +1,8 @@
 import axios from "axios";
 
-function create () {
-  const http = axios.create({
-    baseURL: "http://localhost:8080/api",
-    headers: {
-      "Content-type": "application/json"
-    }
-  });
-    http.interceptors.request.use(config => {
-      config.params = {
-        id: "5fc8e14f450e318e34fc4411",
-        password: "123",
-        ...config.params
-      };
-      return config;
-    });
-  return http
-}
+export default axios.create({
+  baseURL: "http://localhost:8080/api",
+  headers: {
+    "Content-type": "application/json"
+  }
+});

9fab361bee7a7a1375736726fbd2e7f0d817915e
Rodrigo Tavares <rodrigo.actavares@gmail.com>
2020-12-03 17:00:46 +0000

    Update http-common.js

--- react-frontend/src/http-common.js
+++ react-frontend/src/http-common.js
@@ -1,8 +1,19 @@
 import axios from "axios";
 
-export default axios.create({
-  baseURL: "http://localhost:8080/api", //check
-  headers: {
-    "Content-type": "application/json"
-  }
-});
+function create () {
+  const http = axios.create({
+    baseURL: "http://localhost:8080/api",
+    headers: {
+      "Content-type": "application/json"
+    }
+  });
+    http.interceptors.request.use(config => {
+      config.params = {
+        id: "5fc8e14f450e318e34fc4411",
+        password: "123",
+        ...config.params
+      };
+      return config;
+    });
+  return http
+}
